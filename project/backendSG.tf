da4ca5a568b09043a085ddeae2f6653291354635
mditsolutions1 <mahesh@mditsolutions.in>
2022-05-02 16:37:59 +0000

    added project terraform files

---
+++ project/backendSG.tf
@@ -0,0 +1,27 @@
+# Create Database Security Group
+
+resource "aws_security_group" "backendsg" {
+  name        = "Backend SG"
+  description = "Allow inbound traffic from application layer"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  ingress {
+      description     = "Allow traffic from application layer"
+      from_port       = 8080
+      to_port         = 8080
+      protocol        = "tcp"
+      security_groups = [aws_security_group.frontendsg.id]
+  }
+
+  egress {
+      from_port   = 8989
+      to_port     = 8989
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+        Name = "Backend SG"
+  }
+}
+
