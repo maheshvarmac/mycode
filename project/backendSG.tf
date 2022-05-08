93d116af99ce621ebd9a3bac6b35a0ce558a89fc
mditsolutions1 <mahesh@mditsolutions.in>
2022-05-08 16:47:10 +0000

    updated state file

--- project/backendSG.tf
+++ project/backendSG.tf
@@ -13,6 +13,14 @@ resource "aws_security_group" "backendsg" {
       security_groups = [aws_security_group.frontendsg.id]
   }
 
+# SSH access from anywhere
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
       from_port   = 8989
       to_port     = 8989
