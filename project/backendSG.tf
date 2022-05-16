36e5e7c44f5267560110e05cf8d45de8c83f479f
mditsolutions1 <mahesh@mditsolutions.in>
2022-05-16 12:12:27 +0000

    Terraform Security Groups updated

--- project/backendSG.tf
+++ project/backendSG.tf
@@ -5,27 +5,33 @@ resource "aws_security_group" "backendsg" {
   description = "Allow inbound traffic from application layer"
   vpc_id      = aws_vpc.project_vpc.id
 
-  ingress {
-      description     = "Allow traffic from application layer"
-      from_port       = 8080
-      to_port         = 8080
-      protocol        = "tcp"
-      security_groups = [aws_security_group.frontendsg.id]
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
+# HTTPS access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 # SSH access from anywhere
-    ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+# Outbound Rules
+  # Internet access to anywhere
   egress {
-      from_port   = 8989
-      to_port     = 8989
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
